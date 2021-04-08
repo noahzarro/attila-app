369ba7c72c9c50e5df06b22e7797758dc72d9390
David Schober <dasch00@gmx.ch>
2021-04-08 09:14:27 +0200

    added pictures

--- Attila/src/app/tab1/tab1.page.ts
+++ Attila/src/app/tab1/tab1.page.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  frontimage = "";
+  bieber = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.y-jFGr4P0vbUEKqC35tXnQAAAA%26pid%3DApi&f=1";
+  faader = "https://attila-teufen.weebly.com/uploads/2/6/6/5/26659704/6529744_orig.jpg";
+  wolfe = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.PtOv_9-DFFpYhdJ2DwyUzAHaEK%26pid%3DApi&f=1";
+  lumpe = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.f20aXx9YDGAp7TSjTOo5zwAAAA%26pid%3DApi&f=1";
   constructor() {}
 
+  changeImage(stufe){
+    this.frontimage = stufe;
+  }
 }

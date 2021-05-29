4e2bf3e68cc4d5d9800f5b175d95dc2abf3b3086
zarron <zarron@student.ethz.ch>
2021-05-29 15:38:07 +0000

    added types

---
+++ Attila/src/app/misc/types.ts
@@ -0,0 +1,11 @@
+export type Activity = {
+    id: number,
+    name: string,
+    date: string,
+    end_date: string,
+    time: string,
+    place: string,
+    take_away: string,
+    other: string,
+    image: string
+}
